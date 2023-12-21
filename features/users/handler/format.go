b37b323eff79aef8942297f295df4894d3872216
imDzed <mhafidz.hidayat@gmail.com>
2023-12-21 11:57:08 +0700

    BE19 : Project : Get User By Skill

--- features/users/handler/format.go
+++ features/users/handler/format.go
@@ -48,11 +48,35 @@ type UserResponse struct {
 	Alamat   string    `json:"alamat" form:"alamat"`
 	Foto     string    `json:"foto" form:"foto"`
 	Role     string    `json:"role" form:"role"`
-	// Skills   UserSkill `json:"skills" form:"skills"`
 	Skill    []string  `json:"skill" form:"skill"`
+	Skilll UserSkill
+}
+
+type UserResponseUpdate struct {
+	ID       uint      `json:"id" form:"id"`
+	UserName string    `json:"username" form:"username"`
+	Nama     string    `json:"nama" form:"nama"`
+	Email    string    `json:"email" form:"email"`
+	NoHp     string    `json:"nohp" form:"nohp"`
+	Alamat   string    `json:"alamat" form:"alamat"`
+	Foto     string    `json:"foto" form:"foto"`
+	Role     string    `json:"role" form:"role"`
+	Skill    []UserSkill  `json:"skill" form:"skill"`
+}
+
+type GetUserResponse struct {
+	ID       uint      `json:"id" form:"id"`
+	UserName string    `json:"username" form:"username"`
+	Nama     string    `json:"nama" form:"nama"`
+	Email    string    `json:"email" form:"email"`
+	Alamat   string    `json:"alamat" form:"alamat"`
+	Foto     string    `json:"foto" form:"foto"`
+	Skill    []UserSkill  `json:"skill" form:"skill"`
 }
 
 type UserSkill struct {
 	SkillID   uint   `json:"skill_id"`
 	NamaSKill string `json:"skill"`
 }
+
+
